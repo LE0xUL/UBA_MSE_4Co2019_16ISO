8a110206f47681945a08ebbcb624eb490e8ab3d7
Leonardo Urrego <trecetp@gmail.com>
2019-03-20 04:03:36 -0500

    Cambio de contexto entre Task1 y Task2 funcionando

--- TP00/src/main.c
+++ TP00/src/main.c
@@ -28,19 +28,13 @@
 static void initHardware(void);
 
 /** @brief Inicializa pila de tarea
- *	@return none
+ *	@return puntero de pila
  */
-static uint32_t* iniStackTask( uint32_t *stack, uint32_t sizeStack , taskFunction_t functionName , void *argFunction );
+static uint32_t* iniStackTask( uint32_t *stack , taskFunction_t functionName , void *argFunction );
 
-/** @brief delay function
- * @param t desired milliseconds to wait
- */
-// static void pausems(uint32_t t);
 
 /*==================[internal data definition]===============================*/
 
-/** @brief used for delay counter */
-// static uint32_t pausems_count;
 
 /*==================[external data definition]===============================*/
 
@@ -61,7 +55,7 @@ static void initHardware(void)
 	SysTick_Config(SystemCoreClock / 1000);			// Se fija a 1mS
 }
 
-void taskVoid( void *arg )
+void taskVoid()
 {
 	while( 1 )
 	{
@@ -69,68 +63,87 @@ void taskVoid( void *arg )
 	}
 }
 
-static uint32_t* iniStackTask( uint32_t *stack , uint32_t sizeStack , taskFunction_t functionName , void *argFunction )
+static uint32_t* iniStackTask( uint32_t *stack , taskFunction_t functionName , void *argFunction )
 {
-	// bzero( stack , size );
-	stack += sizeStack-1;
-	*stack-- = 1 << 24;			// xPSR
+	*stack-- = 1 << 24;					// xPSR
 	*stack-- = (uint32_t)functionName;	// PC
 	*stack-- = (uint32_t)taskVoid;		// LR
-	*stack-- = 0;				// R12
-	*stack-- = 0;				// R3
-	*stack-- = 0;				// R2
-	*stack-- = 0;				// R1
-	*stack-- = (uint32_t)argFunction;		// R0
-	*stack-- = 0;				// R11
-	*stack-- = 0;				// R10
-	*stack-- = 0;				// R9
-	*stack-- = 0;				// R8
-	*stack-- = 0;				// R7
-	*stack-- = 0;				// R6
-	*stack-- = 0;				// R5
-	*stack = 0;				// R4
+	*stack-- = 0;						// R12
+	*stack-- = 0;						// R3
+	*stack-- = 0;						// R2
+	*stack-- = 0;						// R1
+	*stack-- = (uint32_t)argFunction;	// R0
 
-	return stack;
+	*stack-- = 0xFFFFFFF9;				// LR IRQ
+	*stack-- = 0;						// R11
+	*stack-- = 0;						// R10
+	*stack-- = 0;						// R9
+	*stack-- = 0;						// R8
+	*stack-- = 0;						// R7
+	*stack-- = 0;						// R6
+	*stack-- = 0;						// R5
+	*stack-- = 0;						// R4
+
+	return ++stack;
 }
-
-
-// static void pausems(uint32_t t)
-// {
-// 	pausems_count = t;
-// 	while (pausems_count != 0) {
-// 		__WFI();
-// 	}
-// }
 
 /*==================[external functions definition]==========================*/
 
-// void SysTick_Handler(void)
-// {
-// 	if(pausems_count > 0) pausems_count--;
-// }
-
-void* task1 ( void *arg )
+void* 		task1( void *arg )
 {
 	Board_LED_Toggle(LED);
+	while (1)
+		__WFI();
 	return 0;
 }
 
-void* task2 ( void *arg )
+void* 		task2( void *arg )
 {
 	Board_LED_Toggle(LED);
+	while (1)
+		__WFI();
 	return 0;
+}
+
+uint32_t*	getNextSP( uint32_t *currentSP )
+{
+	uint32_t *nextSP;
+	switch( currentTask )
+	{
+		case 0:
+			nextSP = sp1;
+			currentTask = 1;
+		break;
+
+		case 1:
+			sp1 = currentSP;
+			nextSP = sp2;
+			currentTask = 2;
+		break;
+
+		case 2:
+			sp2 = currentSP;
+			nextSP = sp1;
+			currentTask = 1;
+		break;
+
+		default:
+			while (1)
+				__WFI();
+	}
+
+	return nextSP;
 }
 
 int main(void)
 {
-	sp1 = iniStackTask( stackTask1 , _STACK_SIZE_ , task1 , (void *)0x11223344 );
+	sp1 = iniStackTask( &stackTask1[ _STACK_SIZE_ - 1 ] , task1 , (void *)0x11223344 );
+	sp2 = iniStackTask( &stackTask2[ _STACK_SIZE_ - 1 ] , task2 , (void *)0x11223344 );
 
 	initHardware();
 
 	while (1)
 	{
-		// Board_LED_Toggle(LED);
-		// pausems(DELAY_MS);
 		__WFI();
 	}
 }
