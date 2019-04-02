29dee6125cc3eee623ae04879a7ab475d452accd
Leonardo Urrego <trecetp@gmail.com>
2019-04-02 13:01:17 -0500

    Manejo de tareas con politica RoundRobin para tareas de igual prioridad

--- TP00/src/main.c
+++ TP00/src/main.c
@@ -86,9 +86,9 @@ int main(void)
 
 	tosIniOs_v();
 
-	tosAddTask_ui32( &stackTask1[ _DEFAULT_STACK_SIZE_ - 1 ] , task1 , (void *)0x11223344 , _TOS_TASK_PRIORITY_0_);
+	tosAddTask_ui32( &stackTask1[ _DEFAULT_STACK_SIZE_ - 1 ] , task1 , (void *)0x11223344 , _TOS_TASK_PRIORITY_2_);
 	tosAddTask_ui32( &stackTask2[ _DEFAULT_STACK_SIZE_ - 1 ] , task2 , (void *)0x11223344 , _TOS_TASK_PRIORITY_1_);
-	tosAddTask_ui32( &stackTask3[ _DEFAULT_STACK_SIZE_ - 1 ] , task3 , (void *)0x11223344 , _TOS_TASK_PRIORITY_2_);
+	tosAddTask_ui32( &stackTask3[ _DEFAULT_STACK_SIZE_ - 1 ] , task3 , (void *)0x11223344 , _TOS_TASK_PRIORITY_0_);
 
 	tosIniSchedule_v();
 
--- TP00/src/trecOS.c
+++ TP00/src/trecOS.c
@@ -35,10 +35,11 @@ static 	void* 		idleTask 		( void* );
 
 /*==================[internal data definition]===============================*/
 
-static 	taskStatus_t		taskStatus[ _TOS_MAX_TASK_ ];
+static 	taskStatus_t	taskStatus[ _TOS_MAX_TASK_ ];
 static 	taskData_t		taskData[ _TOS_MAX_TASK_ ];
 
 static	uint32_t		stackIdleTask[ _TOS_IDLE_TASK_STACK_SIZE_ ];
+static	uint8_t			roundRobinControl[ _TOS_TASK_PRIORITY_IDLE_ + 1 ];		// Almacena el index de la ultima tarea ejecutada por cada nivel de prioridad
 
 /*==================[external data definition]===============================*/
 
@@ -75,7 +76,7 @@ void SysTick_Handler()
 {
 	// Escanea las tareas en estado wait
 	for( uint8_t i = 0 ; i < _TOS_MAX_TASK_ ; i++ )
-		if( taskStatus[ i ].state == _TOS_TASK_STATE_WAIT_ && --taskData[ i ].delayTime == 0)
+		if( taskStatus[ i ].state == _TOS_TASK_STATE_WAIT_ && --taskData[ i ].delayTime == 0 )
 			taskStatus[ i ].state 	= _TOS_TASK_STATE_READY_;
 
 	schedule();
@@ -126,10 +127,16 @@ uint32_t*	getNextSP( uint32_t *currentSP )
 	}
 
 	// Busca la siguiente tarea en READY por orden de prioridad
-	for( int pry = _TOS_TASK_PRIORITY_0_ ; pry <= _TOS_TASK_PRIORITY_IDLE_ ; pry++)
+	for( int pry = _TOS_TASK_PRIORITY_0_ ; pry <= _TOS_TASK_PRIORITY_IDLE_ ; pry++ )
 	{
-		for( int idx = _TOS_IDLE_TASK_ID_INDEX_ ; idx < _TOS_MAX_TASK_ ; idx++ )
-		{
+		// int idx = roundRobinControl[ pry ] + 1 == _TOS_MAX_TASK_ ? _TOS_IDLE_TASK_ID_INDEX_ : roundRobinControl[ pry ] + 1 ;
+		int idx = roundRobinControl[ pry ] + 1 ;
+		if ( idx == _TOS_MAX_TASK_ )
+			idx = _TOS_IDLE_TASK_ID_INDEX_ ;
+
+		int endIndex = idx;
+
+		do {
 			if( taskStatus[ idx ].state == _TOS_TASK_STATE_READY_ &&
 				taskStatus[ idx ].priority == pry )
 			{
@@ -137,16 +144,36 @@ uint32_t*	getNextSP( uint32_t *currentSP )
 				nextSP = taskData[ idx ].pStack;
 				tosData.idCurrentTask = taskData[ idx ].id;
 				taskStatus[ idx ].state = _TOS_TASK_STATE_RUN_;
+				roundRobinControl[ pry ] = idx;
 				return nextSP;
 			}
-		}
+
+			if ( ++idx == _TOS_MAX_TASK_ )
+				idx = _TOS_IDLE_TASK_ID_INDEX_ ;
+		} while ( idx != endIndex);
 	}
 
-	// Si no encuentra tarea, ejecuta la IdleTask
-	nextSP = taskData[ _TOS_IDLE_TASK_ID_INDEX_ ].pStack;
-	tosData.idCurrentTask = _TOS_IDLE_TASK_ID_INDEX_;
-	tosData.indexCurrentTask = _TOS_IDLE_TASK_ID_INDEX_;
-	taskStatus[ _TOS_IDLE_TASK_ID_INDEX_ ].state = _TOS_TASK_STATE_RUN_;
+	// for( int pry = _TOS_TASK_PRIORITY_0_ ; pry <= _TOS_TASK_PRIORITY_IDLE_ ; pry++ )
+	// {
+	// 	for( int idx = _TOS_IDLE_TASK_ID_INDEX_ ; idx < _TOS_MAX_TASK_ ; idx++ )
+	// 	{
+	// 		if( taskStatus[ idx ].state == _TOS_TASK_STATE_READY_ &&
+	// 			taskStatus[ idx ].priority == pry )
+	// 		{
+	// 			tosData.indexCurrentTask = idx;
+	// 			nextSP = taskData[ idx ].pStack;
+	// 			tosData.idCurrentTask = taskData[ idx ].id;
+	// 			taskStatus[ idx ].state = _TOS_TASK_STATE_RUN_;
+	// 			return nextSP;
+	// 		}
+	// 	}
+	// }
+
+	// Si no encuentra tarea, ejecuta la IdleTask (En teoria esto nunca se debe ejecutar pero se deja por seguridad)
+	nextSP 											= taskData[ _TOS_IDLE_TASK_ID_INDEX_ ].pStack;
+	tosData.idCurrentTask 							= _TOS_IDLE_TASK_ID_INDEX_;
+	tosData.indexCurrentTask 						= _TOS_IDLE_TASK_ID_INDEX_;
+	taskStatus[ _TOS_IDLE_TASK_ID_INDEX_ ].state 	= _TOS_TASK_STATE_RUN_;
 
 	return nextSP;
 }
